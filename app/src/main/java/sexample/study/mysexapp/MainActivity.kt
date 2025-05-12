ecb492bbac7e5e739e3ddb6ece5bc374bc1ec1db
Zakharov Roman <ambeadexter@yandex.ru>
2025-05-13 03:36:43 +0700

    Was solved error with a build operation failed

--- app/src/main/java/sexample/study/mysexapp/MainActivity.kt
+++ app/src/main/java/sexample/study/mysexapp/MainActivity.kt
@@ -3,7 +3,6 @@ package sexample.study.mysexapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlin.properties.Delegates
 
 const val TAG = "MainActivity"
 
